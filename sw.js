688db6082c52f38964b40950c0fe69b45c5fa535
lz <3600778334@qq.com>
2019-02-18 09:37:06 +0800

    Site updated: 2019-02-18 09:37:01

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","2d10a89feded9348a8de56b523c7aeb9"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
